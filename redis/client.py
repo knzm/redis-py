1794cb2276931e1505a068a26b555bc9be66b296
Nozomu Kaneko <nozom.kaneko@gmail.com>
2011-11-07 21:32:36 +0900

    fix NameError

--- redis/client.py
+++ redis/client.py
@@ -241,7 +241,7 @@ class Redis(threading.local):
             self.connection.disconnect()
             # Connect manually here. If the Redis server is down, this will
             # fail and raise a ConnectionError as desired.
-            connection.connect()
+            self.connection.connect()
             self.connection.send(command, self)
             if subscription_command:
                 return None
